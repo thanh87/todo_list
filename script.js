a69d23575d90c4f8a9bc039547b3b6829b43724f
thanh87 <azura19871@gmail.com>
2017-08-03 12:19:39 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 $(document).ready(function(){
+    
     $("#button").click(function(){
         var toAdd=$("input[name=checkListItem]").val();
         $(".list").append("<div class='item'>"+toAdd+"</div>");
-        });
+    });
+    
     $(document).on("click", ".item", function(){
         $(this).remove();
-        });
     });
+});
